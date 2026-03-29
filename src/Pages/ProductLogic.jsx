62a9409ed3f7f79bb4d5255c6cd5f1b002fbf372
malaika-zafar123 <malaikazafar236@gmail.com>
2026-03-29 10:08:56 -0700

    commit

--- src/Pages/ProductLogic.jsx
+++ src/Pages/ProductLogic.jsx
@@ -4,21 +4,23 @@ import ProductTable from "./ProductTable";
 export default function ProductLogic() {
   const [products, setProducts] = useState([]);
   const [search, setSearch] = useState("");
+useEffect(() => {
+  const saved = JSON.parse(localStorage.getItem("products"));
 
-  useEffect(() => {
-    const saved = JSON.parse(localStorage.getItem("products"));
-
-    if (saved) {
-      setProducts(saved);
-    } else {
-      fetch("https://dummyjson.com/products?limit=20")
-        .then((res) => res.json())
-        .then((data) => {
-          setProducts(data.products);
-          localStorage.setItem("products", JSON.stringify(data.products));
-        });
-    }
-  }, []);
+  if (saved && saved.length > 0) {
+    setProducts(saved);
+  } else {
+    fetch("https://dummyjson.com/products?limit=20")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data.products);
+        localStorage.setItem("products", JSON.stringify(data.products));
+      })
+      .catch((err) => {
+        console.log("Error fetching:", err);
+      });
+  }
+}, []);
 
   return (
     <ProductTable
